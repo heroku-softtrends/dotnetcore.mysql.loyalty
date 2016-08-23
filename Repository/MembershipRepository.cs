8c7c3c556146c852b4d781e6764a37eceb94a02a
tamilavel <tamilavels@softtrends.com>
2016-08-23 21:12:54 +0530

    commit 5

--- Repository/MembershipRepository.cs
+++ Repository/MembershipRepository.cs
@@ -13,7 +13,8 @@ namespace Loyalty.Repository
     {
         //public string _connectionString = "host=localhost;port=3306;database=loyalty;uid=root;pwd=password;";
         //public string _connectionString = "host=209.198.193.73;port=3800;database=loyalty;uid=root;pwd=password;";
-        public string _connectionString = "host=us-cdbr-iron-east-04.cleardb.net;port=3306;database=heroku_0aaed3e5ac3fbc1;uid=b2fc2195b8e3a5;pwd=642f1680662180d;";
+        //public string _connectionString = "host=us-cdbr-iron-east-04.cleardb.net;port=3306;database=heroku_0aaed3e5ac3fbc1;uid=b2fc2195b8e3a5;pwd=642f1680662180d;";
+        public string _connectionString = Environment.GetEnvironmentVariable("ConnectionString");
         public List<Membership> GetMembership(string _userid)
         {
             List<Membership> lstMembership = new List<Membership>();
